385e1e965384619e643bd6d868b8e5c3a0176a1b
Angel Ignatov <angel.hr.ignatov@gmail.com>
2016-01-26 21:07:59 +0200

    default template changed

--- layout/default/page/default.blade.php
+++ layout/default/page/default.blade.php
@@ -1,16 +1,9 @@
 @extends('atlantis::page/shell')
 
-  @section('content')
-  <div id='header'>
-    
-    {!! trans('site::messages.welcome'); !!}
-    
-     {!! $header !!}
-  </div>
-  <div id="content">
-    {!! $content !!}
-    </div>
-  <div id="footer">
-    {!! $footer !!}
-    </div>  
+@section('content')
+
+<div id="content">
+  {!! $content !!}
+</div>
+
 @stop  
