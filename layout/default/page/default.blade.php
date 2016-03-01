7cfc45c13845ce547885ce82d524807416d57af2
Angel Ignatov <angel.hr.ignatov@gmail.com>
2016-03-02 00:07:23 +0200

    page templates update

--- layout/default/page/default.blade.php
+++ layout/default/page/default.blade.php
@@ -1,9 +1,24 @@
 @extends('atlantis::page/shell')
 
-@section('content')
+@section('tracking_header')
+@parent
+{{-- Add tracking header per template --}}
+@stop
 
+@section('scripts')
+@parent
+{{-- Add scripts per template --}}
+{{-- <script src="http://a3.angel.dev.gentecsys.net/media/js/vendor/jquery.js"></script> --}}
+@stop
+
+@section('styles')
+@parent
+{{-- Add styles per template --}}
+@stop
+
+@section('content')
+@parent
 <div id="content">
   {!! $content !!}
 </div>
-
 @stop  
