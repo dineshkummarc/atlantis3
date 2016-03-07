136b78efbd3b2a79b77cb478d77dbb72174681c7
Angel Ignatov <angel.hr.ignatov@gmail.com>
2016-03-07 20:36:03 +0200

    OpenGraph Module update

--- modules/atlantis/opengraph/src/Module/OpenGraph/Events/OpenGraphEvent.php
+++ modules/atlantis/opengraph/src/Module/OpenGraph/Events/OpenGraphEvent.php
@@ -16,17 +16,55 @@ class OpenGraphEvent extends \Illuminate\Support\Facades\Event {
 
   public function setTags($oPage) {
 
-    //dd($oPage->title);
-    
+    $oMedia = \Atlantis\Models\Repositories\MediaRepository::getImage($oPage->preview_thumb_id);
+
     $a = \App::make('Assets');
-    
-    $a->registerHeadTag('<meta property="og:title" content="' . $oPage->seo_title . '" />');    
-    
+
+    $this->openGraph($oPage, $oMedia, $a);
+
+    $this->twitterTags($oPage, $oMedia, $a);
   }
 
   public function subscribe($events) {
-    
+
     $events->listen('page.loaded', 'Module\OpenGraph\Events\OpenGraphEvent@setTags');
+  }
+
+  private function openGraph($oPage, $oMedia, $a) {
+
+    if ($oMedia != NULL) {
+      $image = url(config('user_media_upload') . $oMedia->original_filename);
+    }
+
+    /**
+     * Pinterest need to use <meta property="og:type" content="article" />
+     */
+    $a->registerHeadTag('<meta property="og:type" content="website" />');
+    //$a->registerHeadTag('<meta property="og:type" content="article" />');
+    
+    $a->registerHeadTag('<meta property="og:title" content="' . htmlentities($oPage->seo_title) . '" />');
+    $a->registerHeadTag('<meta property="og:description" content="' . htmlentities($oPage->meta_description) . '" />');    
+    $a->registerHeadTag('<meta property="og:url" content="' . request()->url() . '" />');
+    $a->registerHeadTag('<meta property="og:site_name" content="' . htmlentities(config('site_name')) . '" />');
+    if (isset($image)) {
+      $a->registerHeadTag('<meta property="og:image" content="' . $image . '" />');
+    }
+  }
+
+  private function twitterTags($oPage, $oMedia, $a) {
+    
+     if ($oMedia != NULL) {
+      $image = url(config('user_media_upload') . $oMedia->original_filename);
+    }
+    
+    $a->registerHeadTag('<meta name="twitter:card" content="summary_large_image">');
+    $a->registerHeadTag('<meta name="twitter:site" content="@' . str_replace(' ', '', htmlentities(config('site_name'))) . '">');
+    $a->registerHeadTag('<meta name="twitter:creator" content="@' . str_replace(' ', '', htmlentities($oPage->author)) . '">');
+    $a->registerHeadTag('<meta name="twitter:title" content="' . htmlentities($oPage->seo_title) . '" />');
+    $a->registerHeadTag('<meta name="twitter:description" content="' . htmlentities($oPage->meta_description) . '" />');
+    if (isset($image)) {
+      $a->registerHeadTag('<meta name="twitter:image" content="' . $image . '" />');
+    }
   }
 
 }
