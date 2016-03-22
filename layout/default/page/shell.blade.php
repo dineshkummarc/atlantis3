aa3fa8ff0ab46df5654f0a5098a5b589405253ce
Angel Ignatov <angel.hr.ignatov@gmail.com>
2016-03-22 23:51:22 +0200

    shortcut bar on pages added

--- layout/default/page/shell.blade.php
+++ layout/default/page/shell.blade.php
@@ -9,7 +9,7 @@
     @foreach($_headTags as $tag)
     {!! $tag !!}
     @endforeach
-    @show    
+    @show
 
     @section('scripts')
     @foreach($_scripts as $script)
@@ -25,6 +25,45 @@
 
   </head>
   <body class="{!! $body_class !!}">
+
+    <div class="admin-shortcut">
+       <link rel="stylesheet" href="vendor/atlantis-labs/atlantis3-framework/src/Atlantis/Assets/css/admin-bar.css"> 
+        <div class="row">
+            <div class="columns">
+                <div class="top-bar" id="user-menu">
+                    <div class="top-bar-left user-menu">
+                        <div class="account">
+                            <span class="icon icon-User left"></span>
+                            <div class="username">Milen</div>
+                            <div class="actions">
+                                <a href="#">Setings</a> / <a href="#">Loogout</a>
+                            </div>
+                        </div>
+                        <h3 class="menu-text left">Site Title</h3>
+                    </div>
+                    <span data-responsive-toggle="main-nav">
+                        <span class="menu-icon dark" data-toggle></span>
+                    </span>
+                    <div id="main-nav" class="top-bar-right">
+                        <ul class="dropdown menu" data-dropdown-menu>
+                            <li class="active"><a href="#">Pages</a></li>
+                            <li><a href="#">Patterns</a></li>
+                            <li><a href="#">Modules</a>
+                                <ul class="menu vertical">
+                                    <li><a href="#">One</a></li>
+                                    <li><a href="#">Two</a></li>
+                                    <li><a href="#">Three</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="icon icon-Tools"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>       
+    </div>
     
     @section('tracking_header')
     {!! $tracking_header !!}
