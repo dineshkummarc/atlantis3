8008683905ecaf774eaa28592856992bace8e6f0
Angel Ignatov <angel.hr.ignatov@gmail.com>
2016-06-03 23:18:53 +0300

    404 soundex added

--- resources/themes/theme101/views/page/404.blade.php
+++ resources/themes/theme101/views/page/404.blade.php
@@ -2,7 +2,19 @@
 
 
 @section('content')
+@parent
 
-We can't find this page!
+@if (Session::has('data') && count(Session::get('data.results')) > 0)
+<h1>Did you mean:</h1> 
+@foreach (Session::get('data.results') as $res)
+<p>
+  <a target="blank" href="{!! $res['url'] !!}">{{ $res['name'] }}</a>
+</p>
+@endforeach
+@else
+<h1>Sorry, can't find this page</h1>
+@endif
+
+{{ $content }}
 
 @stop
