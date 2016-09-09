0fbbb7649beeebb37bb27381daf9d246f311822a
Angel Ignatov <angel.hr.ignatov@gmail.com>
2016-09-09 22:31:38 +0300

    password reset added

--- resources/themes/theme101/views/auth/password.blade.php
+++ resources/themes/theme101/views/auth/password.blade.php
@@ -13,8 +13,15 @@
 <div class="row">
   <div class="columns large-3 large-centered text-center">
     <div style="height:120px;line-height:120px;"></div>
-    <img src="vendor/atlantis-labs/atlantis3-framework/src/Atlantis/Assets/images/atlantis_logo.png" alt="">
+    <img src="/vendor/atlantis-labs/atlantis3-framework/src/Atlantis/Assets/images/atlantis_logo.png" alt="">
     <br><br>
+    
+    @if(Session::has('status'))
+    <div class="callout success">
+      {{ Session::get('status') }}
+    </div>
+    @endif
+    
     @if( $errors->all() ) 
     @foreach($errors->all() as $error)
     <div class="callout alert">
@@ -27,20 +34,15 @@
 
 
 <div id='loginForm'>
-  {!! Form::open(["url" => "admin" . $urlQuery]) !!} 
+  {!! Form::open(["url" => "admin/password/email"]) !!} 
   <div class="row">
     <div class="columns large-3 large-centered text-center">
       <div class="input-group">
-        <span class="icon icon-User input-group-label"></span> 
-        {!! Form::input("text", "username", '' , array("id" => "username","class"=>"input-group-field", "placeholder" => "Username")) !!}
+        <span class="icon icon-Mail input-group-label"></span>
+        {!! Form::input("email", "email",  old('email'), array("id" => "email","class"=>"input-group-field", "placeholder" => "Email")) !!}
       </div>
-      <div class="input-group">
-        <span class="icon icon-Key input-group-label"></span>
-        {!! Form::input("password", "password", '' , array("id" => "password","class"=>"input-group-field", "placeholder" => "Password")) !!}
-      </div>
-      
-      {!! Form::submit('Login',['class'=>'alert button expanded']) !!}
-      <a href="#">Lost your password?</a>
+
+      {!! Form::submit('Send Password Reset Link',['class'=>'alert button expanded']) !!}
 
     </div>
   </div>
